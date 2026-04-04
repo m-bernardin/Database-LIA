e3628dbb20bf15beab258647050d85fb8ec853dd
m-bernardin <mjrb4mjrb@gmail.com>
2026-04-03 20:27:13 -0400

    InsuranceCompany + Patient tabel creation script

--- Deliverable2/script.sql
+++ Deliverable2/script.sql
@@ -1,13 +1,23 @@
 CREATE DATABASE DoctorsOffice;
 USE DoctorsOffice;
 
+CREATE TABLE InsuranceCompany(
+    companyID INT CONSTRAINT PK_InsuranceCompany PRIMARY KEY,
+    name VARCHAR(30),
+    city VARCHAR(30)
+);
+CREATE TABLE Patient(
+    patientID INT IDENTITY CONSTRAINT PK_Patient PRIMARY KEY,
+    fName VARCHAR(20),
+    lName VARCHAR(20),
+    email VARCHAR(40),
+    address VARCHAR(40)
+);
+CREATE TABLE Dentist;
+CREATE TABLE Appointment;
+CREATE TABLE Visit;
 CREATE TABLE VisitDetails;
 CREATE TABLE Bill;
-CREATE TABLE Visit;
-CREATE TABLE Appointment;
-CREATE TABLE InsuranceCompany;
-CREATE TABLE Patient;
 CREATE TABLE Claim;
 CREATE TABLE Payment;
 CREATE TABLE PaysFor;
-CREATE TABLE Dentist;
